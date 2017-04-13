68578426a87af6251c21b0ac8015b9def7f68b49
John Bachman <bachmanjohn@gmail.com>
2017-04-13 13:05:14 -0400

    Sort dict by PMID to make data reproducible

--- step4_stmt_entity_stats/get_stats.py
+++ step4_stmt_entity_stats/get_stats.py
@@ -4,15 +4,20 @@ if __name__ == '__main__':
     from indra.preassembler import grounding_mapper as gm
     from indra.tools.reading import reading_results_stats as rrs
 
-    #filename = '../step3_sample_training_test/training_pmid_stmts.pkl'
-    filename = '../step3_sample_training_test/test_pmid_stmts.pkl'
+    filename = '../step3_sample_training_test/training_pmid_stmts.pkl'
+    #filename = '../step3_sample_training_test/test_pmid_stmts.pkl'
     with open(filename, 'rb') as f:
         pmid_stmts = pickle.load(f)
     print("No. of papers in training set: %d" % len(pmid_stmts.keys()))
 
-    stmts = [s for stmt_list in pmid_stmts.values() for s in stmt_list]
+    # Sort the statements by PMID key
+    sorted_pmids = sorted(pmid_stmts.keys())
+    stmts = []
+    for pmid in sorted_pmids:
+        for s in pmid_stmts[pmid]:
+            stmts.append(s)
 
-    gm.raw_agent_texts(stmts, 10000, 'raw_agent_texts.csv')
+    gm.raw_agent_texts(stmts, 10000, 'raw_agent_texts_before.csv')
 
     rrs.report_grounding(stmts, bin_interval=100,
                          plot_prefix='training')
