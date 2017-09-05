ea34cc30ab132d44f720fd69364d5819a05396cf
Ben Gyori <ben.gyori@gmail.com>
2017-09-04 22:03:06 -0400

    Parameterize get agent links and add PubMed links

--- step4_stmt_entity_stats/generate_agent_links.py
+++ step4_stmt_entity_stats/generate_agent_links.py
@@ -1,8 +1,7 @@
+import os
+import sys
 import csv
 from indra.databases import get_identifiers_url
-
-sample_file = 'training_agents_sample_curated.csv'
-out_file = 'training_agents_sample_links.html'
 
 def get_link(db_name, db_ref):
     url = get_identifiers_url(db_name, db_ref)
@@ -10,12 +9,28 @@ def get_link(db_name, db_ref):
     return link
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: %s [training|test]' % os.path.basename(__file__))
+        sys.exit()
+    mode = sys.argv[1]
+    if mode not in ('training', 'test'):
+        print('Usage: %s [training|test]' % os.path.basename(__file__))
+        sys.exit()
+
+    sample_file = 'training_agents_sample_curated.csv' if mode == 'training' \
+        else 'test_agents_with_be_sample.csv'
+    out_file = 'training_agents_sample_links.html' if mode == 'training' \
+            else 'test_agents_with_be_sample.html'
+
     with open(sample_file, 'r') as fh1, open(out_file, 'w') as fh:
         reader = csv.reader(fh1)
         next(reader)
         fh.write('<body><table border=1>')
         for row in reader:
             fh.write('<tr><td>%s</td>' % row[2])
+            link = 'https://www.ncbi.nlm.nih.gov/pubmed/%s' % row[9]
+            fh.write('<td><a href="%s" target="_blank">%s</a></td>' %
+                     (link, row[9]))
             for i in range(3):
                 db_name = row[2*i+3]
                 if db_name == 'IPR':
