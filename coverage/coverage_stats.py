cb12b99885b303b798d2f92e1b0d9e2ae57ed5dd
Ben Gyori <ben.gyori@gmail.com>
2017-10-03 14:48:59 -0400

    Refactor coverage stats to separate reusable parts

--- coverage/coverage_stats.py
+++ coverage/coverage_stats.py
@@ -1,41 +1,13 @@
 import os
 import sys
-import csv
 import numpy
 from indra.util import plot_formatting as pf
 import matplotlib.pyplot as plt
 from matplotlib_venn import venn3
 from matplotlib.patches import Circle
+from util import *
 
 bepath = '../../../../bioentities'
-
-def read_csv(fh, delimiter, quotechar):
-    if sys.version_info.major < 3:
-        csvreader = csv.reader(fh, delimiter=bytes(delimiter),
-                               quotechar=bytes(quotechar))
-    else:
-        csvreader = csv.reader(fh, delimiter=delimiter, quotechar=quotechar)
-    rows = [row for row in csvreader]
-    return rows
-
-def load_csv(filename):
-    with open(filename) as f:
-        rows = read_csv(f, ',', '"')
-    return rows
-
-def load_entity_list(filename):
-    with open(filename) as f:
-        rows = read_csv(f, ',', '"')
-    entities = [row[0] for row in rows]
-    return entities
-
-def load_equivalences(filename):
-    equivalences = []
-    with open(filename) as f:
-        rows = read_csv(f, ',', '"')
-        for row in rows:
-            equivalences.append((row[0], row[1], row[2]))
-    return equivalences
 
 def find_db_mappings(entity, equivalences):
     dbs = []
---
+++ coverage/util.py
@@ -0,0 +1,34 @@
+import csv
+
+
+def read_csv(fh, delimiter, quotechar):
+    if sys.version_info.major < 3:
+        csvreader = csv.reader(fh, delimiter=bytes(delimiter),
+                               quotechar=bytes(quotechar))
+    else:
+        csvreader = csv.reader(fh, delimiter=delimiter, quotechar=quotechar)
+    rows = [row for row in csvreader]
+    return rows
+
+
+def load_csv(filename):
+    with open(filename) as f:
+        rows = read_csv(f, ',', '"')
+    return rows
+
+
+def load_entity_list(filename):
+    with open(filename) as f:
+        rows = read_csv(f, ',', '"')
+    entities = [row[0] for row in rows]
+    return entities
+
+
+def load_equivalences(filename):
+    equivalences = []
+    with open(filename) as f:
+        rows = read_csv(f, ',', '"')
+        for row in rows:
+            equivalences.append((row[0], row[1], row[2]))
+    return equivalences
+
